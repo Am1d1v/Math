056c05eaf505731418749c543726fbded9d5a988
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-10 19:51:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ console.log(Number.isNaN(10 / 0));
 console.log((10 / 0));
 console.log(isFinite(10 / 0));
 console.log(isFinite(10 / 3));
+
+console.log(Number.isInteger(10));
+console.log(Number.isInteger(10.5));
+
+
+//========= Math ============================
+
+console.log(Math.sqrt(36));
+console.log(Math.sqrt(10));
+
+console.log(Math.cbrt(27));
+
+console.log(16 ** (1 / 4));
+
+console.log(Math.sign(-100));
+console.log(Math.sign(30));
+
+console.log(Math.abs(-8));
+
+console.log(Math.exp(2));
+
+console.log(Math.max(1, -2, 10, 0 ,19));
+console.log(Math.min(1, -2, 10, 0 ,19));
