fc629b77898f33ee52d9e7b09f93513acd32b9aa
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-10 19:24:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+console.log(10 === 10.0);
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+
+
+console.log(Number("10"));
+console.log(+"10");
+console.log(Number.parseInt("10"));
+console.log(Number.parseInt("11", 10));
