1a36a9b23760e7acf51ee651446a8fa8405fe963
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-10 19:33:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ console.log(Number("10"));
 console.log(+"10");
 console.log(Number.parseInt("10"));
 console.log(Number.parseInt("11", 10));
+
+console.log(Number.parseInt("11.5"));
+console.log(Number.parseFloat("11.5"));
+console.log(Number.parseInt("11.5 sec"));
+console.log(Number.parseInt("sec 11"));
+
+
+console.log(Number.isNaN("10dddddd"));
+console.log(Number.isNaN(Number("10dddddd")));
+console.log(Number.isNaN(10));
+console.log(Number.isNaN(Number("A")));
+console.log(Number.isNaN("A"));
+console.log(Number.isNaN(10 / 0));
+console.log((10 / 0));
